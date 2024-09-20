13fdf1fede4450f80917c15b132c9dd205b79d44
tvas538 <tvansivo@gmail.com>
2024-09-20 06:20:49 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+$name="<h1> TUMWIJUKYE IVAN </h2>";
+$DOB="<h2>11/09/2002</h2>";
+$Address="<h2>KOKOBA MBARARA</h2>";
+$CYear=2024;
+$BYear=2000;
+$Welcome="<h1>IVAN</h1>";
+$Age=$CYear-$BYear;
+echo"<h1>Name: </h1>".$name. "<br>";
+echo"<h2>Date Of Birth : </h2>".$DOB."<br>";
+echo"<h2>Home Address:</h2> ".$Address."<br>";
+echo"<h3>Welcome home:</h3> ".$Welcome."<br>";
+echo"You are now , ".$Age." Years old";
+/* DOB represents date of birth,
+CYEAR means current year
+BYEAR means year of birth*/
+?> 
+
